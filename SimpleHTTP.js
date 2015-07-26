d4d9876e0b4d5b3122a86a57f6b275659502b8ec
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-07-26 11:10:36 -0700

    fixed port collision error

--- SimpleHTTP.js
+++ SimpleHTTP.js
@@ -1,12 +1,13 @@
-var http = require('http'); //load http and fs modules for noe
+var http = require('http'); //load http and fs modules for node
 var fs = require('fs');
 
-var port = 8001;
+var port = 1234; //if err thrown, port is likely already in use
 var localhost = '127.0.0.1';
 
 var server = http.createServer(function(request, response){
+	//create server using http module
 
-	fs.exists("index.html", function(err){
+	fs.exists("index.html", function(err){ //file system module
 		try {
 			if(!err){ //if no error
 				response.writeHead(200, {"Content-Type": "text/html"});
@@ -20,14 +21,14 @@ var server = http.createServer(function(request, response){
 				response.write("<h2>Proudly running on " + localhost + ":" + port + "</h2>");
 				response.write("<p>This site is a mockup for the Javascript Development Accelerator Challenge</p>");
 				response.write("<a href='https://www.codefellows.org/'>Visit CodeFellows</a>");
-				response.write("<p></p>")
+				response.write("<p></p>");
 				response.write("<img src='https://pbs.twimg.com/profile_images/269279233/llama270977_smiling_llama_400x400.jpg'/>");
 				response.write("</body>");
 				response.write("</html>");
 				response.end();
 
-			} else { //error code
-				response.writeHead(500);
+			} else { //if error
+				response.writeHead(500); //generic server error
 				response.end();
 			}
 		} catch (err) {
@@ -36,5 +37,5 @@ var server = http.createServer(function(request, response){
 	})
 })
 
-server.listen(port, localhost);
+server.listen(port, localhost); //server listener
 console.log('Server is running on: ' + localhost + ':' + port);
